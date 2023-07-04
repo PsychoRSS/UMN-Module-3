b056263aff18f583c978b2bf7b1292870788d3f6
PsychoRSS <Bobgonzalez516@gmail.com>
2023-07-03 20:04:06 -0600

    added specific functions for specific situatuations

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,83 @@
 // Assignment code here
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// var passwordCriteria = {
-//   alphabet: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-//   speialChar: ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', '\\', '/', '<', '>', '?', '.', ',', ':', ';'],
-//   numbers: [0,1,2,3,4,5,6,7,]
-// }
 var AlphabetCriteria = 'abcdefghijklmnopqrstuvwxyz';
 var specialChar = "!@#$%^&*()-_+=[{]}\|;:',<.>/?"
 var number = "0123456789"
 
+var generateBtn = document.querySelector("#generate");
 
-var alphaConfirm = window.confirm("Would you like uppercase letters")
-if (alphaConfirm) {
-  generateAlpha()
+
+generateBtn.addEventListener('click', function() {
+  var alphaConfirm = window.confirm("Would you like letters")
+  var NumConfirm = window.confirm("Would you like to use numbers?")
+  var SpecCharConfirm = window.confirm("Would you like to use special characters?")
+  var passLength = Number(window.prompt("How long would you like the password to be?"))
+
+  console.log(passLength)
+
+ if (alphaConfirm && NumConfirm && SpecCharConfirm) {
+  generatePassword()
+  
+ } else if (alphaConfirm && NumConfirm) {
+  generateAlphabet()
+  generateNum()
+  console.log()
+ } else if (alphaConfirm && SpecCharConfirm) {
+  var result = generateAlphabet() && generateRandoChar()
+  generateRandoChar()
+ 
+ } else if (NumConfirm && SpecCharConfirm) {
+  generateNum()
+  generateRandoChar()
+ }
+
+  
+
+})
+
+// This function will return a random number between 0-9
+function generateNum() {
+  var randoNum = Math.floor(Math.random() * number.length)
+  var resNum = number[randoNum]
+  
+  return resNum
+}
+
+// This will return a random charater from the AlphabetCriteria string
+function generateAlphabet() {
+  var randoChar = Math.floor(Math.random() * AlphabetCriteria.length)
+  var resChar = AlphabetCriteria[randoChar]
+  
+  return resChar
+}
+
+// This will return a random special charater from the specialChar string
+function generateRandoChar() {
+  var randoSpecChar = Math.floor(Math.random() * specialChar.length)
+  var resSpecChar = specialChar[randoSpecChar]
+
+  return resSpecChar
 }
 
 
-function generateAlpha() {
+function generatePassword() {
+  var randoChar = Math.floor(Math.random() * AlphabetCriteria.length)
+  var randoNum = Math.floor(Math.random() * number.length)
+  var randoSpecChar = Math.floor(Math.random() * specialChar.length)
+
+
+  var resChar = AlphabetCriteria[randoChar]
+  var resNum = number[randoNum]
+  var resSpecChar = specialChar[randoSpecChar]
+
+  var result = resChar + '' + resNum + '' + resSpecChar 
+
+
+ return result
   
 }
 
-function generatePassword() {
-  var rando = Math.floor(Math.random() * passwordCriteria.alphabet.map )
-  var randoNum = Math.floor(Math.random() * passwordCriteria.numbers.length)
-  var randoSpecChar = Math.floor(Math.random() * passwordCriteria.speialChar.length)
-
-  var  all =  passwordCriteria.alphabet.concat(passwordCriteria.speialCharm, passwordCriteria.numbers)
-  console.log(randoSpecChar)
-  console.log(randoNum)
-  console.log(rando)
-  console.log(all)
-}
-
-generatePassword()
+// console.log(generatePassword())
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
